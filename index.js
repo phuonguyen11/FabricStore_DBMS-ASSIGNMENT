52ab2e146e4a8a3b1d4b6c2b05f59dd0020bb069
phuonguyen11 <phuonguyen11112002@gmail.com>
2022-12-05 16:58:53 +0700

    successfully make auto slider banner whoohoo

---
+++ index.js
@@ -0,0 +1,13 @@
+$(document).ready(function()
+{
+//banner owl carousel
+$("#banner-area .owl-carousel").owlCarousel({
+    items:1,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:5000,
+    dots:true,
+
+    });
+});
